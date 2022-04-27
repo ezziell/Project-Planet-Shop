51a60a92476eeacc9761f0ae560e5f7655c263af
eugenia <eugeniaalonsoc@gmail.com>
2022-04-27 13:55:24 -0300

    Avances en ProgressBar y arreglé scss conflictivo

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -6,15 +6,30 @@ const ProgressBar = () => {
     return(
         <body>
         <form action='#' className='form'>
-            <div className='form-step form-step-active'>
+
+          <div className='all-progress'></div>
                 <div className='edit-order-div'>
+                    
                 <StartOrder />
                 </div>
-                <div className='btns-group'>
+
+                < div className='progressbar'>
+                <div className='progress' id='progress'></div>
+            
+
+                <div 
+                className='progress-step progress-step-active' 
+                data-title='intro'>
+                </div>
+                <div className='progress-step' data-title='delivery'></div>
+                <div className='progress-step' data-title='payment'></div>
+                <div className='progress-step' data-title='confirmation'></div>
+                
+            </div>
+                    <div className='btns-group'>
                     <a href="#" className="btn btn-back">Volver</a>
                     <a href="#" className="btn btn-next">Siguiente</a>
                 </div>
-            </div>
             <div className='form-step form-step-active'>
                 <div className='edit-envio-div'>
 
